f484f477c2aec1ec2613e40ec708713ee02fb381
MATHEUS VIEIRA <matheus.vieira137@etec.sp.gov.br>
2025-06-19 21:33:40 -0300

    Feat: Pagina de Login feita

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
+  standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  errorVisib = true;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({      
+      email: ['', [Validators.required, Validators.email]], // O campo deve ter um formato de e-mail válido (ex: usuario@dominio.com).
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      //Acessar a Home
+      alert("Formulário válido!");
+    }
+  }
 }

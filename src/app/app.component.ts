ab3ca58266ae54eac66177ac0bb35edf6d1e0dcb
MATHEUS VIEIRA <matheus.vieira137@etec.sp.gov.br>
2025-06-20 12:43:26 -0300

    Feat: Header feito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { LoginComponent } from "./login/login.component";
 import { HomeComponent } from "./home/home.component";
 import { SobreComponent } from "./sobre/sobre.component";
+import { ContatoComponent } from "./contato/contato.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent, HomeComponent, SobreComponent],
+  imports: [LoginComponent, HomeComponent, SobreComponent, ContatoComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
